3aa4860590d39499fac02c8f96a76d60de088385
sriafifa <chi606951@gmail.com>
2026-04-19 23:38:12 +0700

    untuk memenuhi tugas pakedwin laravel-per10

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,13 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.scss',
-                'resources/js/app.js',
-            ],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+        tailwindcss(),
     ],
-    resolve: {
-        alias: {
-            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-            '~bootstrap-icons': path.resolve(__dirname, 'node_modules/bootstrap-icons'),
-        }
-    },
 });
